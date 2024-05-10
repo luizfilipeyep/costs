0777968e76da5176a546269c9c15c8a3c9ca1d9f
Luiz Omena <luizfilipeyep@gmail.com>
2024-05-10 13:52:18 -0300

    post

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,13 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 import "../layout/style.css"
 import ProjectForm from "../project/ProjectForm";
 
 function NewProject() {
+  const history = useNavigate()
+
+  const createPost = (project) => {
+    project.cost = 0
+    project.services = []
+
+    // fetch("http://localhost:8080/projects", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-type": "application/json"
+    //   },
+    //   body: JSON.stringify(project)
+    // }).then(() => {
+    //   navigate("/projects", { message: "Projeto criado com sucesso!" })
+    // })
+
+
+    
+  }
+
   return ( 
     <>
       <section className="newProjectContainer">
         <h1>Criar Projeto</h1>
         <p>Crie seu projeto para depois adicionar os serviços</p>
-        <ProjectForm btnText="Criar projeto" />
+        <ProjectForm handleSubmit={createPost} btnText="Criar projeto" />
       </section>      
     </>
    );
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -5,8 +5,9 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ btnText, handleSubmit, projectData }) {
   const [categories, setCategories] = useState([])
+  const [project, setProject] = useState(projectData || [])
 
   useEffect(() => {
     axios
@@ -16,27 +17,58 @@ function ProjectForm({ btnText }) {
       })
   },[])
 
+  const submit = (e) => {
+    e.preventDefault()
+    // console.log(project)
+    handleSubmit(project)
+
+    axios
+      .post("http://localhost:8080/projects", {
+        body: JSON.stringify(project)
+      })
+      .then(
+        navigate("/projects", { message: "Projeto criado com sucesso!" })
+      )
+  }
+
+  const handleChange = (e) => {
+    setProject({ ...project, [e.target.name]: e.target.value })
+  }
+
+  const handleCategory = (e) => {
+    setProject({ ...project, category: {
+      id: e.target.value,
+      name: e.target.options[e.target.selectedIndex]
+    } })
+  }
+
   console.log(categories)
 
   return ( 
     <>
-      <form action="" method="get">
+      <form onSubmit={submit} action="" method="get">
         <Input 
           type="text" 
           text="Nome do projeto"
           name="name"
           placeholder="Insira o nome do projeto"
+          handleOnChange={handleChange}
+          value={project.name ? project.name : ""}
         />
         <Input 
           type="number" 
           text="Orçamento do projeto"
           name="budget"
           placeholder="Insira o orçamento total"
+          handleOnChange={handleChange}
+          value={project.budget ? project.budget : ""}
         />
         <Select 
           name="category_id"
           text="Selecione a categoria" 
           options={categories}
+          handleOnChange={handleCategory}
+          value={project.category ? project.category.id : ""}
         />
         <SubmitButton text={btnText} />
       </form>
